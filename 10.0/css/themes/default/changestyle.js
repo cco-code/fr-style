edf5d27dbe3488a6424a6c1e6c43bbd183bd7454
jamesoo760 <53468216+jamesoo760@users.noreply.github.com>
2019-08-07 16:34:02 +0800

    Add files via upload

---
+++ 10.0/css/themes/default/changestyle.js
@@ -0,0 +1,24 @@
+
+function changeStyle() {
+  $("[widgetname^='LABEL_BIGTITLE']").addClass("label-bigtitle");
+  $("[widgetname^='COMBOBOX_CHOICEDOWN']").addClass("comboBox-choicedown");
+  $("[widgetname^='BTN_DONE']").children().addClass("btn-done");
+  $("[widgetname^='BTN_NORMAL']").children().addClass("btn-normal");
+  $("[widgetname^='BTN_BORDER']").children().addClass("btn-border");
+  $("[widgetname^='LABEL_NORMAL']").addClass("label-normal");
+  $("[widgetname^='LABEL_BORDER']").addClass("label-border");
+  $("[widgetname^='LABEL_TOPTITLE']").addClass("label-toptitle");
+  $("[widgetname^='LABEL_BODY']").addClass("label-body");
+  $("[widgetname^='LABEL_TITLEBAR']").addClass("label-titlebar");
+  $("[widgetname^='LABEL_BGNM']").addClass("label-bgnm");
+  $("[widgetname^='LABEL_BGBD']").addClass("label-bgbd");
+  $("[widgetname^='TEXTEDITOR_NORMAL']").addClass("textEditor-normal");
+  $("[widgetname^='COMBOBOX_NORMAL']").addClass("comboBox-normal");
+
+  $("[widgetname^='CHART']").children().addClass("chart");
+  $("[widgetname^='CHART_PIE']").children().addClass("chart-pie");
+  $("[widgetname^='CHART_AREA']").children().addClass("chart-area");
+
+  $("[widgetname*='SEMICIRCLE']").children().addClass("semicircle");
+  $("[widgetname*='BDRS0']").children().addClass("bdrs0");
+};
