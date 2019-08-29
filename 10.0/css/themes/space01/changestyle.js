b6e2febf960eff18566c0b6578c6269764068ae3
jamesoo760 <53468216+jamesoo760@users.noreply.github.com>
2019-08-29 18:00:57 +0800

    Add files via upload

--- 10.0/css/themes/space01/changestyle.js
+++ 10.0/css/themes/space01/changestyle.js
@@ -8,15 +8,80 @@ function changeStyle() {
   $("[widgetname^='LABEL_NORMAL']").addClass("label-normal");
   $("[widgetname^='LABEL_BORDER']").addClass("label-border");
   $("[widgetname^='LABEL_TOPTITLE']").addClass("label-toptitle");
+  $("[widgetname^='LABEL_REPORTTITLE']").addClass("label-reporttitle");
   $("[widgetname^='LABEL_BODY']").addClass("label-body");
   $("[widgetname^='LABEL_TITLEBAR']").addClass("label-titlebar");
   $("[widgetname^='LABEL_BGNM']").addClass("label-bgnm");
   $("[widgetname^='LABEL_BGBD']").addClass("label-bgbd");
+  $("[widgetname^='LABEL_REPORTBG']").addClass("label-reportbg");
+  $("[widgetname^='LABEL_MIDDLE']").addClass("label-middle");
+  $("[widgetname^='LABEL_SUBTITLE']").addClass("label-subtitle");
+  $("[widgetname^='LABEL_BG']").addClass("label-bg");
   $("[widgetname^='TEXTEDITOR_NORMAL']").addClass("textEditor-normal");
-  $("[widgetname^='COMBOBOX_NORMAL']").addClass("comboBox-normal");
+  
+  $("[widgetname^='NUMBEREDITOR_END']").addClass("numberEditor-end");
+  
+  
+  $("[widgetname^='CHECKBOX']").addClass("checkBox");
+  $("[widgetname^='CHECKBOX_SLIM']").addClass("checkBox-slim");
+  $("[widgetname^='CHECKBOX_SQURE']").addClass("checkBox-squre");
 
+  $("[widgetname^='CHART']").children().addClass("chart");
   $("[widgetname^='CHART_PIE']").children().addClass("chart-pie");
+  $("[widgetname^='CHART_AREA']").children().addClass("chart-area");
+
+  $("[widgetname^='REPORT']").addClass("report");
+  $("[widgetname^='REPORT_BORDER']").addClass("report-border");
 
   $("[widgetname*='SEMICIRCLE']").children().addClass("semicircle");
   $("[widgetname*='BDRS0']").children().addClass("bdrs0");
+
+
+  $("[widgetname^='BODY']").addClass("body");
+  $("[widgetname^='LABEL']").addClass("label");
+  $("[widgetname^='BUTTON']").children().addClass("button");
+
+  $("[widgetname^='COMBOBOX']").addClass("comboBox");
+  $("[widgetname^='COMBOCHECKBOX']").addClass("comboCheckBox");
+  $("[widgetname^='DATEEDITOR']").addClass("dateEditor");
+  $("[widgetname^='CBG']").addClass("checkBoxGroup");
+  $("[widgetname^='RADIOGROUP']").addClass("radioGroup");
+
+  $("[widgetname^='TEXTEDITOR']").addClass("textEditor");
+  $("[widgetname^='NUMBEREDITOR']").addClass("numberEditor");
+  $("[widgetname^='TEXTAREA']").addClass("textArea");
+  $("[widgetname^='PASSWORD']").addClass("password");
+
+  $("[widgetname^='TREEEDITOR']").addClass("treeEditor");
+  $("[widgetname^='TCBE']").addClass("treeComboBoxEditor");
+  $("[widgetname^='MFU']").addClass("multiFileUploader");
+
+  $("[widgetname^='REPORT']").addClass("report");
+  $("[widgetname^='TABLAYOUT']").addClass("tabLayout");
+
+  $(".fr-core-window").children().addClass("wAlpha");
+  $(".fr-core-window").html("<div class='wAlpha'></div>");
+
+
+
+  $("[widgetname^='CHECKBOX_BGBLUE'] > .fr-checkbox-checkoff").click(
+    function(){
+      $("[widgetname^='CHECKBOX_BGBLUE']").toggleClass("bgBlue");
+    }
+  );
+  $("[widgetname^='CHECKBOX_SLIM_BGRED'] > .fr-checkbox-checkoff").click(
+    function(){
+      $("[widgetname^='CHECKBOX_SLIM_BGRED']").toggleClass("bgRed");
+    }
+  );
+  $("[widgetname^='CHECKBOX_SQURE_BGORANGE'] > .fr-checkbox-checkoff").click(
+    function(){
+      $("[widgetname^='CHECKBOX_SQURE_BGORANGE']").toggleClass("bgOrange");
+    }
+  );
+  $("[widgetname^='CHECKBOX_BGGREEN'] > .fr-checkbox-checkoff").click(
+    function(){
+      $("[widgetname^='CHECKBOX_BGGREEN']").toggleClass("bgGreen");
+    }
+  );
 };
