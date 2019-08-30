21601164fa18bebfdb7e87d814e5bf07817e6115
jamesoo760 <53468216+jamesoo760@users.noreply.github.com>
2019-08-30 17:54:15 +0800

    Add files via upload

---
+++ 10.0/css/themes/default/changeColor.js
@@ -0,0 +1,14 @@
+function changeColor() {
+  $("[widgetname*='RED']").addClass("red");
+  $("[widgetname*='ORANGE']").addClass("orange");
+  $("[widgetname*='YELLOW']").addClass("yellow");
+  $("[widgetname*='GREEN']").addClass("green");
+  $("[widgetname*='PEACOCK']").addClass("peacock");
+  $("[widgetname*='SKY']").addClass("sky");
+  $("[widgetname*='BLUE']").addClass("blue");
+  $("[widgetname*='PURPLE']").addClass("purple");
+  $("[widgetname*='LIGHTGRAY']").addClass("lightgray");
+  $("[widgetname*='DARKGRAY']").addClass("darkgray");
+  $("[widgetname*='WHITE']").addClass("white");
+  $("[widgetname*='BLACK']").addClass("black");
+};
--- 10.0/css/themes/default/changestyle.js
+++ 10.0/css/themes/default/changestyle.js
@@ -6,7 +6,7 @@ function changeStyle() {
   $("[widgetname^='BTN_NORMAL']").children().addClass("btn-normal");
   $("[widgetname^='BTN_BORDER']").children().addClass("btn-border");
   $("[widgetname^='LABEL_NORMAL']").addClass("label-normal");
-  $("[widgetname^='LABEL_BORDER']").addClass("label-border");
+  
   $("[widgetname^='LABEL_TOPTITLE']").addClass("label-toptitle");
   $("[widgetname^='LABEL_REPORTTITLE']").addClass("label-reporttitle");
   $("[widgetname^='LABEL_BODY']").addClass("label-body");
@@ -16,12 +16,15 @@ function changeStyle() {
   $("[widgetname^='LABEL_REPORTBG']").addClass("label-reportbg");
   $("[widgetname^='LABEL_MIDDLE']").addClass("label-middle");
   $("[widgetname^='LABEL_SUBTITLE']").addClass("label-subtitle");
+  $("[widgetname^='LABEL_BG']").addClass("label-bg");
   $("[widgetname^='TEXTEDITOR_NORMAL']").addClass("textEditor-normal");
-  $("[widgetname^='NUMBEREDITOR']").addClass("numberEditor");
+  
   $("[widgetname^='NUMBEREDITOR_END']").addClass("numberEditor-end");
-  $("[widgetname^='COMBOBOX_NORMAL']").addClass("comboBox-normal");
-  $("[widgetname^='COMBOCHECKBOX_NORMAL']").addClass("comboCheckBox-normal");
+  
+  
   $("[widgetname^='CHECKBOX']").addClass("checkBox");
+  $("[widgetname^='CHECKBOX_SLIM']").addClass("checkBox-slim");
+  $("[widgetname^='CHECKBOX_SQURE']").addClass("checkBox-squre");
 
   $("[widgetname^='CHART']").children().addClass("chart");
   $("[widgetname^='CHART_PIE']").children().addClass("chart-pie");
@@ -32,4 +35,58 @@ function changeStyle() {
 
   $("[widgetname*='SEMICIRCLE']").children().addClass("semicircle");
   $("[widgetname*='BDRS0']").children().addClass("bdrs0");
+  
+
+  $("[widgetname^='BODY']").addClass("body");
+
+  $("[widgetname^='LABEL']").addClass("label");
+  $("[widgetname^='LABEL_BORDER']").addClass("label-border");
+
+
+  $("[widgetname^='BUTTON']").addClass("button");
+  $("[widgetname^='BUTTON_HALFO']").addClass("buttonHalfO");
+
+  $("[widgetname^='COMBOBOX']").addClass("comboBox");
+  $("[widgetname^='COMBOCHECKBOX']").addClass("comboCheckBox");
+  $("[widgetname^='DATEEDITOR']").addClass("dateEditor");
+  $("[widgetname^='CBG']").addClass("checkBoxGroup");
+  $("[widgetname^='RADIOGROUP']").addClass("radioGroup");
+
+  $("[widgetname^='TEXTEDITOR']").addClass("textEditor");
+  $("[widgetname^='NUMBEREDITOR']").addClass("numberEditor");
+  $("[widgetname^='TEXTAREA']").addClass("textArea");
+  $("[widgetname^='PASSWORD']").addClass("password");
+
+  $("[widgetname^='TREEEDITOR']").addClass("treeEditor");
+  $("[widgetname^='TCBE']").addClass("treeComboBoxEditor");
+  $("[widgetname^='MFU']").addClass("multiFileUploader");
+
+  $("[widgetname^='REPORT']").addClass("report");
+  $("[widgetname^='TABLAYOUT']").addClass("tabLayout");
+
+  $(".fr-core-window").children().addClass("wAlpha");
+  $(".fr-core-window").html("<div class='wAlpha'></div>");
+
+
+
+  $("[widgetname^='CHECKBOX_BGBLUE'] > .fr-checkbox-checkoff").click(
+    function(){
+      $("[widgetname^='CHECKBOX_BGBLUE']").toggleClass("bgBlue");
+    }
+  );
+  $("[widgetname^='CHECKBOX_SLIM_BGRED'] > .fr-checkbox-checkoff").click(
+    function(){
+      $("[widgetname^='CHECKBOX_SLIM_BGRED']").toggleClass("bgRed");
+    }
+  );
+  $("[widgetname^='CHECKBOX_SQURE_BGORANGE'] > .fr-checkbox-checkoff").click(
+    function(){
+      $("[widgetname^='CHECKBOX_SQURE_BGORANGE']").toggleClass("bgOrange");
+    }
+  );
+  $("[widgetname^='CHECKBOX_BGGREEN'] > .fr-checkbox-checkoff").click(
+    function(){
+      $("[widgetname^='CHECKBOX_BGGREEN']").toggleClass("bgGreen");
+    }
+  );
 };
